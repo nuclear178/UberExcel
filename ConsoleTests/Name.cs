0ce094d3744de5582983f0957abdd55699103b31
manager <alex.l@udrive.ua>
2018-10-25 13:14:31 +0300

    Implemented serialization; tested;

--- ConsoleTests/Name.cs
+++ ConsoleTests/Name.cs
@@ -5,6 +5,6 @@ namespace ConsoleTests
     public class Name
     {
         [Column(1)] public string First { get; set; } //TODO :: add offset
-        [Column(2)] public string Last { get; set; }
+        [Column(3)] public string Last { get; set; }
     }
 }
--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Reflection;
-using ExcelTools.IO;
+using System.Collections.Generic;
+using System.IO;
+using ExcelTools;
+using OfficeOpenXml;
 
 namespace ConsoleTests
 {
@@ -23,30 +25,71 @@ namespace ConsoleTests
             //}
 
 
-            var analyzer = new AttributeBasedIntrospector(typeof(Fuel));
-            Console.WriteLine($"Columns number: {analyzer.Analyze()}");
+            /*var analyzer = new AttributeBasedIntrospector(typeof(Fuel));
+            Console.WriteLine($"Columns number: {analyzer.Analyze()}");*/
 
             /*Fuel fuelItem = fuels[0];
             object val = GetPropValue("Name.First", fuelItem);
 
             Console.WriteLine(val);*/
+
+            var fuels = new List<Fuel>
+            {
+                new Fuel
+                {
+                    Type = "tp1",
+                    Volume = 5,
+                    Bucket = new Bucket
+                    {
+                        Prop1 = 1, Prop2 = new Name
+                        {
+                            First = "fr1",
+                            Last = "ls1"
+                        }
+                    }
+                },
+                new Fuel
+                {
+                    Type = "tp2",
+                    Volume = 7,
+                    Bucket = new Bucket
+                    {
+                        Prop1 = 2, Prop2 = new Name
+                        {
+                            First = "fr5",
+                            Last = "ls7"
+                        }
+                    }
+                }
+            };
+
+            string fileName = "Example-CRM-" + DateTime.Now.ToString("yyyy-MM-dd--hh-mm-ss") + ".xlsx";
+
+            // Create the file using the FileInfo object
+            var file = new FileInfo(fileName);
+            using (var package = new ExcelPackage(file))
+            {
+                var convert = WorksheetConvert<Fuel>.BuildAttributeBased();
+                ExcelWorksheet fuelsWorksheet = package.Workbook.Worksheets.Add("sl");
+                convert.SerializeObject(fuels, fuelsWorksheet);
+
+                package.Save();
+            }
         }
 
-        private static object GetPropValue(string propAddress, object obj)
+        /*private static object GetPropValue(string propAddress, object obj)
         {
             foreach (string part in propAddress.Split('.'))
             {
                 if (obj == null) return null;
-
                 Type type = obj.GetType();
                 PropertyInfo info = type.GetProperty(part);
                 if (info == null) return null;
-
                 obj = info.GetValue(obj, null);
             }
 
             return obj;
-        }
+        }*/
 
         /*Console.WriteLine("::");
 
